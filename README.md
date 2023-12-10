760d730e3dc1ffd44533f418e00348b49b9e239b
thediegobryan <thediegobryan@gmail.com>
2023-12-10 10:11:24 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# recruitingGPT
+# coolstartupname
+This is the guide to set up GPT locally
+https://platform.openai.com/docs/quickstart?context=python
---
+++ sqlite_db_setup.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('student_database.db')
+print("Connected to database successfully")
+
+conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, zip TEXT)')
+print("Created table successfully!")
+
+conn.close()
