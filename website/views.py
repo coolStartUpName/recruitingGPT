4811942d963b70c7d4bf85c82903618dbde42bde
Cole Higgins <chiggins@redhat.com>
2024-01-13 14:23:55 -0500

    comparing vector distances

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 from website import db
 from .models import Applicants
 from .gpt import gptTest
+from .job_embedding import job_embedding
+from scipy.spatial.distance import euclidean
 
 views = Blueprint('views', __name__)
 
@@ -13,18 +15,25 @@ def index():
 def job_description():
     if request.method == 'POST':
         resume = request.form.get('resume')
-        job_description = request.form.get('jobDescription')
-        applicant = Applicants(resume=resume, job_description=job_description)
+        # job_description = request.form.get('jobDescription')
+        resume_embedding = gptTest(resume)
+        distance = euclidean(resume_embedding, job_embedding)
+        string_distance = str(distance)
+        string_resume_embedding = str(resume_embedding)
+        # job_embedding = gptTest(job_embedding)
+        applicant = Applicants(resume=resume, resume_embedding=string_resume_embedding, distance=string_distance)
         db.session.add(applicant)
         db.session.commit()
-        gptTest()
-        return render_template('applicants.html', resume=resume, jd=job_description)
+        applicants_data = Applicants.query.all()
+        return render_template('applicants.html', applicants=applicants_data)
     else:
         return render_template('jobDescription.html')
 
 @views.route("/applicants")
 def applicants():
-    return render_template('applicants.html')
+    applicants_data = Applicants.query.all()
+
+    return render_template('applicants.html', applicants=applicants_data)
 
 
 # new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
